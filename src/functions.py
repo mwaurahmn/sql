210e9941fdf9f3674ad7254cc696c4c0048cf685
mwaurahmn <100678788+mwaurahmn@users.noreply.github.com>
2023-08-18 15:44:13 +0300

    case 2
    case 2 started

--- src/functions.py
+++ src/functions.py
@@ -1,7 +1,15 @@
 # Import Libraries
 import pandas as pd
+import matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+# Visualization display
+sns.set(rc={'figure.figsize':(15,8)}, font_scale = 2)
+sns.set_style('darkgrid')
+matplotlib.rcParams['font.size'] = 16
+matplotlib.rcParams['figure.figsize'] = (15, 8)
+matplotlib.rcParams['figure.facecolor'] = '#00000000'
 
 def run_query(query, connection):
     '''
@@ -24,12 +32,17 @@ def horizontal_barplot(y, x, title, xlabel):
     Takes the x and y variables and plots a horizontal barplot.
     Also takes the title of the plot and the label of the x-axis.
     '''
-    plt.figure(figsize=(15,8))
     plt.barh(y, x)
-    plt.title(title, fontsize=27)
-    plt.xlabel(xlabel, fontsize=19)
-    plt.xticks(fontsize=16)
-    plt.yticks(fontsize=16)
+    plt.title(title)
+    plt.xlabel(xlabel)
     plt.tick_params(left=False)
     sns.despine(left=True)
+    plt.show()
+
+def lineplot(x, y, category, title):
+    '''
+    Takes the variables needed to plot a line graph.
+    '''
+    sns.lineplot(x = x, y = y, hue = category)
+    plt.title(title)
     plt.show()
